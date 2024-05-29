3950406819e8f977833ce5035a888574a7927ae1
C4ioD <caiodias29091@gmail.com>
2024-05-28 21:13:01 -0300

    config tela login

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,6 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class FormLogin(FlaskForm):
-  nome= StringField('nome', validators=[DataRequired()])
-  senha= PasswordField('senha', validators=[DataRequired(), Length(min=6, max=20)])
+  nome= StringField('Nome', validators=[DataRequired()])
+  senha= PasswordField('Senha', validators=[DataRequired(), Length(min=6, max=20)])
   btn_login = SubmitField('Entrar')
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,13 @@
 from app import app
 from flask import render_template, url_for, redirect
+from app.forms import FormLogin
 
 
 @app.route("/")
 def index():
-  
   return render_template('index.html')
+
+@app.route("/login")
+def login():
+  form_login = FormLogin()
+  return render_template('login.html', form_login=form_login)
